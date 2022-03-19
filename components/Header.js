0d5d1cfaf5ac0def1d13cb6b41b4dbccf3b77ab5
shalinikumari123 <70230806+shalini-kumari2000@users.noreply.github.com>
2022-03-19 16:32:11 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -19,7 +19,7 @@ export default function Header() {
 
   return (
     <header>
-      <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-tahiti">
+      <nav className="border-gray-200 px-2 sm:px-4 py-2.5 bg-tahiti">
         <div className="container flex flex-wrap justify-between items-center mx-auto">
           <a href="#" className="flex items-center">
             <img
